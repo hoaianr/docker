a12b031f5bdc8564ba70359cbbf82f23d47a746a
JackyVo <jackyinvn@gmail.com>
2015-11-09 13:14:06 +0700

    Init docker

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '8bc314b66e31f7738e8ddce2ecd9093ee08a31c834a74f6487733b8811365f6a2f5a469a8c2310c90646505ceea38024b11acf8ed593d06598d0d9cdde7d1065'
